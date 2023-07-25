4e7f3c9d01aeac840a8ec848ab84d7f98a80882a
Bepis1559 <axl2001@abv.bg>
2023-07-25 12:42:30 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import "./App.css";
 import { PhotosGrid } from "./components/PhotosGrid";
 import { Skeleton } from "./components/Skeleton";
 import { useFetch } from "./customHooks/useFetch";
-// import { parseLinkHeader } from "./helpers/parseLinkHeader";
-const demoUrl = "http://127.0.0.1:3000/photos-short-list";
 
 function App() {
-  const apiData = useFetch(demoUrl);
+  const url = `http://127.0.0.1:3000/photos?_page=${1}&_limit=${10}`;
+  const apiData = useFetch(url);
   return (
     <>{apiData.isLoading ? <Skeleton /> : <PhotosGrid apiData={apiData} />}</>
   );
